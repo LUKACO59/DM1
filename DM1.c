6e42061a82e51f620a010f9abc128471d36dba69
Luka Collier <98172286+LUKACO59@users.noreply.github.com>
2024-10-23 22:16:17 +0000

    git commit

--- DM1.c
+++ DM1.c
@@ -2,7 +2,17 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <assert.h>
-
+int fexp(int a, int b){
+    int res=1;
+    while(b>0){
+        if(b%2==1){
+            res=res*a;
+        }
+        a=a*a;
+        b=b/2;
+    }
+    return res;
+}
 int mod_exp(int a, int b ,int n){
     int res = 1;
     int amn =a%n;
@@ -28,7 +38,51 @@ bool fermat_primality_test(int n,int k){
     }
     return true;
 }
+void decompose(int n,int *s,int *d){
+    *s=0;
+    *d=n;
+    while(*d%2==0){
+        *s=*s+1;
+        *d=*d/2;
+    }
+}
 
+/*a corriger*/
+bool miller_witness(int n,int a){
+    int *s=malloc(sizeof(int));
+    int *d=malloc(sizeof(int));
+    decompose((n-1),s,d);
+    if(mod_exp(a,(*d),n)==1){
+        free(s);
+        free(d);
+        return true;
+    }
+    int expo=(*d);
+    if(mod_exp(a,expo,n)==(n-1)){
+        free(s);
+        free(d);
+        return true;
+    }
+    for(int i=1;i<(*s);i++){
+        expo=expo*2;
+        if(mod_exp(a,expo,n)==(n-1)){
+            free(s);
+            free(d);    
+            return true;
+        }
+    }
+    free(s);
+    free(d);    
+    return false;
+}
 int main(){
+    int *s=malloc(sizeof(int));
+    int *d=malloc(sizeof(int));
+    int n=13;
+    decompose((n-1),s,d);
+    int res=fexp(2,*s)* (*d);
+    printf("%d\n",res);
+    printf("%d\n",(-1)%2);
+    printf("%d\n",miller_witness(7,3));
     return 0;
 }
